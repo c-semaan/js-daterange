a1e4bca93a1d6a4dad604e22bb8e1c61cc87e4a5
Charles Semaan <Charlessemaan1@gmail.com>
2024-12-17 14:24:31 +0100

    changed return type from string[] to DateRange, added more jsdoc for all methods

--- index.ts
+++ index.ts
@@ -2,7 +2,8 @@ import dayjs from "dayjs";
 import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
 import weekday from "dayjs/plugin/weekday";
-import { DateFormat, DateRange } from "./types/enums";
+import { DateFormat, PresetDateRange } from "./types/enums";
+import { DateRange } from "./types/types";
 dayjs.extend(weekday);
 dayjs.extend(utc);
 dayjs.extend(timezone);
@@ -11,12 +12,13 @@ dayjs.tz.setDefault("UTC");
 class Period {
   timezone?: string;
   utcOffset?: number;
-  dateformat: string;
+  dateformat: DateFormat;
 
   /**
-   *
-   * @param dateformat
-   * @param timezone expects IANA time zone such as "Europe/Paris"
+   * Creates an instance of the Period class to handle date ranges and time zone offsets.
+   * 
+   * @param {DateFormat} dateformat - The format used for date formatting.
+   * @param {string} [timezone] - The IANA time zone string, e.g., "Europe/Paris". If not provided, UTC is used.
    */
   constructor(dateformat: DateFormat, timezone?: string) {
     this.timezone = timezone;
@@ -25,9 +27,11 @@ class Period {
   }
 
   /**
-   *
-   * @param timezone expects IANA time zone such as "Europe/Paris"
-   * @returns {number} the difference in minutes between a specific timezone and Coordinated Universal Time (UTC). Returns 0 if the timezone is invalid.
+   * Calculates the UTC offset based on the given time zone.
+   * 
+   * @param {string} timezone - The IANA time zone string (e.g., "Europe/Paris").
+   * @returns {number} The offset in minutes between the provided time zone and UTC.
+   *                   Returns 0 if the time zone is invalid.
    */
   calculateUtcOffset(timeZone: string): number {
     let result = 0;
@@ -50,7 +54,12 @@ class Period {
     return result;
   }
 
-  getToday(): string[] {
+  /**
+   * Returns the date range for today, from the start of the day (00:00:00) to the end of the day (23:59:59).
+   * 
+   * @returns {DateRange} The start and end dates for today in the specified format.
+   */
+  getToday(): DateRange {
     const today = dayjs()
       .utcOffset(this.utcOffset ?? 0)
       .startOf("day")
@@ -60,17 +69,28 @@ class Period {
       .utcOffset(this.utcOffset ?? 0)
       .endOf("day")
       .format(this.dateformat);
-    return [today, endOfToday];
+    return { start: today, end: endOfToday };
   }
-  getYesterday(): string[] {
+
+  /**
+   * Returns the date range for yesterday, from the start of the day (00:00:00) to the end of the day (23:59:59).
+   * 
+   * @returns {DateRange} The start and end dates for yesterday in the specified format.
+   */
+  getYesterday(): DateRange {
     const yesterday = dayjs()
       .utcOffset(this.utcOffset ?? 0)
       .subtract(1, "day")
       .format(this.dateformat);
-    return [yesterday, yesterday];
+    return { start: yesterday, end: yesterday };
   }
 
-  getLastWeek(): string[] {
+  /**
+   * Returns the date range for last week, from the previous Monday to the previous Sunday.
+   * 
+   * @returns {DateRange} The start and end dates for the previous week in the specified format.
+   */
+  getLastWeek(): DateRange {
     const lastMonday = dayjs()
       .utcOffset(this.utcOffset ?? 0)
       .weekday(-6)
@@ -80,9 +100,15 @@ class Period {
       .weekday(0)
       .format(this.dateformat);
 
-    return [lastMonday, lastSunday];
+    return { start: lastMonday, end: lastSunday };
   }
-  getThisWeek(): string[] {
+
+  /**
+   * Returns the date range for this week, from the current Monday to the current Sunday.
+   * 
+   * @returns {DateRange} The start and end dates for the current week in the specified format.
+   */
+  getThisWeek(): DateRange {
     const currMonday = dayjs()
       .utcOffset(this.utcOffset ?? 0)
       .day(1)
@@ -93,9 +119,15 @@ class Period {
       .day(7)
       .format(this.dateformat);
 
-    return [currMonday, currSunday];
+    return { start: currMonday, end: currSunday };
   }
-  getThisMonth(): string[] {
+
+  /**
+   * Returns the date range for this month, from the first day of the month to the last day of the month.
+   * 
+   * @returns {DateRange} The start and end dates for the current month in the specified format.
+   */
+  getThisMonth(): DateRange {
     const FirstDayOfThisMonth = dayjs()
       .utcOffset(this.utcOffset ?? 0)
       .startOf("month")
@@ -106,10 +138,15 @@ class Period {
       .endOf("month")
       .format(this.dateformat);
 
-    return [FirstDayOfThisMonth, lastDayOfThisMonth];
+    return { start: FirstDayOfThisMonth, end: lastDayOfThisMonth };
   }
 
-  getLastMonth(): string[] {
+  /**
+   * Returns the date range for last month, from the first day of the previous month to the last day of the previous month.
+   * 
+   * @returns {DateRange} The start and end dates for the previous month in the specified format.
+   */
+  getLastMonth(): DateRange {
     const FirstDayOfLastMonth = dayjs()
       .utcOffset(this.utcOffset ?? 0)
       .subtract(1, "month")
@@ -122,25 +159,29 @@ class Period {
       .endOf("month")
       .format(this.dateformat);
 
-    dayjs.tz.guess;
-
-    return [FirstDayOfLastMonth, lastDayOfLastMonth];
+    return { start: FirstDayOfLastMonth, end: lastDayOfLastMonth };
   }
 
-  createDefinedRange(daterange: DateRange): string[] {
+  /**
+   * Returns a date range for a predefined period based on the provided date range.
+   * 
+   * @param {PresetDateRange} daterange - The predefined date range (e.g., LAST_MONTH, THIS_WEEK).
+   * @returns {DateRange} The corresponding start and end dates for the specified period.
+   */
+  createDefinedRange(daterange: PresetDateRange): DateRange {
     const computedDateRange = () => {
       switch (daterange) {
-        case DateRange.LAST_MONTH:
+        case PresetDateRange.LAST_MONTH:
           return this.getLastMonth();
-        case DateRange.LAST_WEEK:
+        case PresetDateRange.LAST_WEEK:
           return this.getLastWeek();
-        case DateRange.THIS_MONTH:
+        case PresetDateRange.THIS_MONTH:
           return this.getThisMonth();
-        case DateRange.THIS_WEEK:
+        case PresetDateRange.THIS_WEEK:
           return this.getThisWeek();
-        case DateRange.TODAY:
+        case PresetDateRange.TODAY:
           return this.getToday();
-        case DateRange.YESTERDAY:
+        case PresetDateRange.YESTERDAY:
           return this.getYesterday();
         default:
           throw new Error(`Unsupported DateRange: ${daterange}`);
@@ -148,15 +189,31 @@ class Period {
     };
     return computedDateRange();
   }
-  createCustomRange(prevDays: number, includingToday: boolean): string[] {
+
+  /**
+   * Generates a date range from the past based on a number of days.
+   * The range can optionally include the current day as the end date.
+   * 
+   * @param {number} prevDays - The number of previous days to calculate the range.
+   * @param {boolean} includingToday - If true, the current day will be included as the end date.
+   * @returns {DateRange} An object containing the formatted start and end dates.
+   * 
+   * @example
+   * const dateRange = createPastDateRange(7, true);
+   * console.log(dateRange); // { start: "2024-12-10", end: "2024-12-17" }
+   */
+  createPastDateRange(prevDays: number, includingToday: boolean): DateRange {
     const endDate = includingToday
       ? dayjs().utcOffset(this.utcOffset ?? 0)
       : dayjs()
-          .utcOffset(this.utcOffset ?? 0)
-          .subtract(1, "day");
+        .utcOffset(this.utcOffset ?? 0)
+        .subtract(1, "day");
 
     const startDate = endDate.subtract(prevDays, "day");
-    return [startDate.format(this.dateformat), endDate.format(this.dateformat)];
+    return {
+      start: startDate.format(this.dateformat),
+      end: endDate.format(this.dateformat),
+    };
   }
 }
 
--- types/enums.ts
+++ types/enums.ts
@@ -1,4 +1,4 @@
-export enum DateRange {
+export enum PresetDateRange {
   TODAY = "TODAY",
   YESTERDAY = "YESTERDAY",
   THIS_WEEK = "THIS_WEEK",
